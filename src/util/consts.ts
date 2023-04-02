7e32b7493cf579e118c5f7cbb3a7f5a2588b4cba
Marinus <vantrexplays@gmail.com>
2023-04-02 06:21:34 +0200

    testing

--- src/util/consts.ts
+++ src/util/consts.ts
@@ -1,5 +1,6 @@
 
-export const API_URL = process.env.REACT_PASTE_API_URL;
+//export const API_URL = process.env.REACT_PASTE_API_URL;
+export const API_URL = 'springpaste-production.up.railway.app';
 
 
 export enum EndPoints {
